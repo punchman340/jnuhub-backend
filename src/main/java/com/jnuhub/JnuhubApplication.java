9b04220756229e5cc652651b18c6ea89558c045f
punchman340 <jmh031016@gmail.com>
2026-03-22 22:33:36 +0900

    feat: enable JPA Auditing

--- src/main/java/com/jnuhub/JnuhubApplication.java
+++ src/main/java/com/jnuhub/JnuhubApplication.java
@@ -1,13 +1,14 @@
 package com.jnuhub;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JnuhubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JnuhubApplication.class, args);
 	}
-
 }
